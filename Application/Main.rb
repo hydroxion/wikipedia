4eb6eb3e397b3847e506952784347846c9c0280b
Sphinx <waltim@outlook.com>
2017-02-02 13:43:40 -0200

    Description of wiki

---
+++ Application/Main.rb
@@ -0,0 +1,70 @@
+
+require 'rubygems'
+
+require 'httparty'
+
+class Wiki
+
+	def initialize (search = "Ruby", lang = "en")
+
+		@search, @lang = search, lang
+
+	end
+
+	def take
+
+		# Error : #{}
+
+		url = "http://dbpedia.org/data/"+@search.gsub(" ", "_")+".json"
+
+		uri = HTTParty.get(url)
+
+	end
+
+	def find_lang (object, lang, result = [])
+
+		object.keys.each do |ky|
+
+			if object[ky].is_a? Hash
+
+				find_lang(object[ky], lang, result)
+
+			elsif object[ky].is_a? Array
+
+				object[ky].each do |i|
+
+					find_lang(i, lang, result) if i.is_a? Hash
+
+				end
+
+			else
+
+				result.push(object) if ky.eql?("lang") && object[ky].eql?(lang)
+
+			end
+
+		end
+
+		result[2]
+
+	end
+
+end
+
+def Clear
+
+	system ("clear || cls")
+
+end
+
+############ -
+
+Clear()
+
+obj = Wiki.new(ARGV[0])
+
+print "\n\n #{obj.find_lang(obj.take, "pt")["value"]}\n\n"
+
+############ -
+
+# Other link' here
---
+++ References.md
@@ -0,0 +1,24 @@
+
+# References
+
+Dependences
+
+	gem install httparty -v 0.14.0
+
+Study
+
+	www.youtube.com/watch?v=BmHKb0kLGtA ( En )
+	
+	www.youtube.com/watch?v=b8xRVxAgknw ( Pt )
+
+	www.youtube.com/watch?v=03VmeD5t3Z0 ( Pt )
+
+	Reading
+		
+		pt.wikipedia.org/wiki/Linked_data
+
+		tutorialspoint.com/json/json_ruby_example.htm
+
+		ruby-doc.org/stdlib-2.0.0/libdoc/json/rdoc/JSON.html
+
+		ruby-doc.org/core-2.4.0/String.html#method-i-chomp-21
